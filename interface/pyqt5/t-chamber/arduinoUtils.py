fbfdb141da7bb27d430f66376e93f8da4b9bfacf
Valentino Glave <defliez@protonmail.com>
2024-10-15 15:14:57 +0200

    Update `get_arduino_boards()` - Update function to only send board name and port, not fqbn.

--- interface/pyqt5/t-chamber/arduinoUtils.py
+++ interface/pyqt5/t-chamber/arduinoUtils.py
@@ -26,9 +26,8 @@ def get_arduino_boards():
                 if "boards" in board and board["boards"]:
                     port = board.get("port", "")
                     board_name = board["boards"][0].get("name", "Unknown Board")
-                    fqbn = board["boards"][0].get("fqbn", "Unknown FQBN")
                     # add (port, board_name, fqbn) to the list
-                    arduino_ports.append((port, board_name, fqbn))
+                    arduino_ports.append((port, board_name))
 
             return arduino_ports
         except json.JSONDecodeError:
