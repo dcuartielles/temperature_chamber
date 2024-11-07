c96bfb6821e9e1e35506c52d097c1ec44567bc39
owenboy9 <owenkraus9@gmail.com>
2024-11-07 16:11:02 +0100

    clean up json functionality

--- interface/pyqt5/t-chamber/jsonFunctionality.py
+++ interface/pyqt5/t-chamber/jsonFunctionality.py
@@ -10,6 +10,7 @@ logger = setup_logger(__name__)
 class FileHandler:
     def __init__(self, config, parent=None):
         self.parent = parent  # optional: parent window for the dialog
+        # class variables
         self.config = config
         self.test_data = None
         self.filepath = None
@@ -17,7 +18,7 @@ class FileHandler:
     # open a file using a file dialog and return the file content
     def open_file(self):
 
-        initial_dir = self.config.get_test_directory()
+        initial_dir = self.config.get_test_directory()  # initially open directory saved in config
 
         # open file dialog to select a JSON file
         filepath, _ = QFileDialog.getOpenFileName(self.parent, "open test file", initial_dir,
@@ -29,15 +30,15 @@ class FileHandler:
                 with open(filepath, mode='r', encoding='utf-8') as input_file:
                     logger.info(filepath)
                     self.test_data = json.load(input_file)  # convert JSON file content to a py dictionary
-                    self.filepath = filepath
+                    self.filepath = filepath  # store file path in class variable
 
                     # update test directory in config to the file's directory
                     test_file_directory = Path(filepath).parent
-                    new_test_directory = test_file_directory.parent
-                    self.config.set_test_directory(new_test_directory)
+                    new_test_directory = test_file_directory.parent  # two levels up from the test file itself
+                    self.config.set_test_directory(new_test_directory)  # update test directory in config
                     logger.info(f"test directory updated to: {new_test_directory} but with correct forward slashes")
 
-                    return self.test_data
+                    return self.test_data  # return test file
 
             except FileNotFoundError:
                 logger.exception(f'file here: {filepath} not found.')
