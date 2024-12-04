941465cfbf837492d05dced17ed256f02c296fe9
Valentino Glave <defliez@protonmail.com>
2024-12-04 13:45:40 +0100

    Fix `get_test_file_name(self)` & `get_test_names_from_queue()` in main.py

--- interface/pyqt5/t-chamber/main.py
+++ interface/pyqt5/t-chamber/main.py
@@ -507,13 +507,21 @@ class MainWindow(QMainWindow):
 
     # retrieve test directory names from test data and send to queue tab
     def get_test_file_name(self):
-        directories = [test["sketch"].split('/')[-2] for test in self.test_data["tests"] if '/' in test["sketch"]]
-        result_string = ",".join(directories)
-        self.queue_tab.get_test_file_name.emit(result_string)
+        try:
+            directories = [
+                test["sketch"].split('/')[-2]
+                for test in self.test_data["tests"].values()
+                if "sketch" in test and '/' in test["sketch"]
+            ]
+            result_string = ",".join(directories)
+            self.queue_tab.get_test_file_name.emit(result_string)
+        except Exception as e:
+            logger.error(f"Error in get_test_file_name: {e}")
+            raise
 
     # get test titles from test data and send to queue tab
     def get_test_names_from_queue(self):
-        names = [test.key() for test in self.test_data["tests"]]
+        names = list(self.test_data["tests"].keys())
         result_string = ",".join(names)
         self.queue_tab.display_queue_from_arduino.emit(result_string)
 
